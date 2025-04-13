b5c6a36ee0626ff4af74e0396153f0cb3890796c
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2025-04-13 14:21:22 +0200

    Initial commit

---
+++ src/ConsoleApplication.java
@@ -0,0 +1,5 @@
+public class ConsoleApplication {
+    public void run() {
+        System.out.println("Running the application");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConsoleApplication application = new ConsoleApplication();
+
+        application.run();
+    }
+}
